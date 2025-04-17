7e5119556aa7726aa28c71e42d43022522c46f90
Tuan Kiet <limit389@gmail.com>
2025-04-17 22:03:59 +0700

    Setup: init-project Summary: - Initialized "device-service" as micro service

---
+++ src/test/java/com/smart_physio_therapy/device_service/DeviceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_physio_therapy.device_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
